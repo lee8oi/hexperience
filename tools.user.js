46d1792f85d43da7c22cb6c17d956403ced9e7be
Lee Forest <lee8oi@gmail.com>
2015-12-14 15:38:00 -0500

    Run at document-end (load quicker).

--- tools.user.js
+++ tools.user.js
@@ -5,6 +5,7 @@
 // @description  Advanced helper tools for Hacker Experience.
 // @author       lee8oi
 // @match        *://hackerexperience.com/*
+// @run-at document-end
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
 // @grant        GM_setValue
 // @grant        GM_getValue
