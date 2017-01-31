e069793d9da3914d3a9481d6da92d99a74a88cf3
Lee Forest <lee8oi@gmail.com>
2017-01-31 14:26:41 -0500

    Version bump

--- tools.user.js
+++ tools.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hexperience Tools
 // @namespace    https://github.com/lee8oi/hexperience
-// @version      0.8.4
+// @version      0.8.5
 // @description  Advanced helper tools for Hacker Experience.
 // @author       lee8oi
 // @match        *://legacy.hackerexperience.com/*
